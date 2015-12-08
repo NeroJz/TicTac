765855b0f2991b4036b796b6750ded25f19e4f54
KumHoe Lau <nerojz86@gmail.com>
2015-12-08 21:24:51 +0800

    Design UI

---
+++ public/app.js
@@ -0,0 +1,25 @@
+var app = angular.module('ticTacApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/login',{
+            templateUrl: 'views/login.view.html'
+        })
+        .when('/register',{
+            templateUrl: 'views/register.view.html'
+        })
+        .when('/users',{
+            templateUrl: 'views/users.view.html'
+        })
+        .when('/gamelist',{
+            templateUrl: 'views/gamelist.view.html'
+        })
+        .when('/leaderboard',{
+            templateUrl: 'views/leaderboard.view.html'
+        })
+        .when('/gameplay',{
+            templateUrl: 'views/gameplay.view.html',
+        });
+
+    $routeProvider.otherwise('/leaderboard');
+}]);
---
+++ public/controllers/gameplay.controller.js
@@ -0,0 +1,61 @@
+angular.module('ticTacApp')
+    .controller('gameplayCtrl', ['$scope',
+        function ($scope) {
+            var turn = 0;
+
+            $scope.currentPlayer = 'X';
+            $scope.winner = null;
+
+            $scope.board = [
+                ['-', '-', '-'],
+                ['-', '-', '-'],
+                ['-', '-', '-'],
+            ];
+
+            $scope.isTaken = function (cell) {
+                return cell !== '-';
+            };
+
+
+            $scope.move = function (row, index) {
+                turn++;
+                $scope.board[row][index] = $scope.currentPlayer;
+                var gameStatus = checkGame($scope.board);
+
+                if(gameStatus){
+                    $scope.winner = $scope.currentPlayer;
+                    alert($scope.winner + " wins.");
+                    return;
+                }
+                if(turn == 9 && gameStatus == false){
+                    alert("Tied game!");
+                    return;
+                }
+
+                $scope.currentPlayer = $scope.currentPlayer === 'X' ? 'O' : 'X';
+            };
+
+            checkGame = function(board){
+                var winner = false;
+
+                for(var i=0 ; i<3 ;i++){
+                    if((board[i][0] !== '-') && (board[i][0] == board[i][1]) && (board[i][1] == board[i][2])){
+                        winner = true;
+                    }
+
+                    if((board[0][i] !== '-') && (board[0][i] == board[1][i]) && (board[1][i] == board[2][i])){
+                        winner = true;
+                    }
+                }
+
+                if((board[0][0] !== '-') && (board[0][0] == board[1][1]) && (board[1][1] == board[2][2])){
+                    winner = true;
+                }
+                if((board[0][2] !== '-') && (board[0][2] == board[1][1]) && (board[1][1] == board[2][0])){
+                    winner = true;
+                }
+
+                return winner;
+            }
+
+        }]);
